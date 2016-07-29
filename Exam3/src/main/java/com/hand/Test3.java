3ad44279714e91a77507ac4e9b23f421011b0ba4
liao1565553 <yuliao.chen@hand-china.com>
2016-07-29 20:47:03 +0800

    修改readme

--- Exam3/src/main/java/com/hand/Test3.java
+++ Exam3/src/main/java/com/hand/Test3.java
@@ -5,49 +5,50 @@ import java.util.Scanner;
 public class Test3 {
 	public static void main(String[] args) {
 		System.out.print("请输入日期：");
-		
+
 		Scanner sc = new Scanner(System.in);
 		String date = sc.next();
 		int year, month, day;
-		int counts=0;
+		int counts = 0;
 
 		try {
 			year = getYear(date);
 			month = getMonth(date);
 			day = getDay(date);
-			if(year<=0){
+			if (year <= 0) {
 				System.err.println("您输入的年不合法");
 			}
-			if(month>12||month<1){
+			if (month > 12 || month < 1) {
 				System.err.println("您输入的月不合法");
 			}
-			if(day>31||day<1){
+			if (day > 31 || day < 1) {
 				System.err.println("您输入的天不合法");
 			}
-			
-			
-			boolean isRyear=false;
-			
-			if(((year%4)==0&&(year%100!=0))||(year%400==0)){
-				isRyear=true;
+
+			boolean isRyear = false;
+
+			if (((year % 4) == 0 && (year % 100 != 0)) || (year % 400 == 0)) {
+				isRyear = true;
 			}
-			
-			int monthOfdays[]={31,28,31,30,31,30,31,31,30,31,30,31};
-			int RmonthOfdays[]={31,29,31,30,31,30,31,31,30,31,30,31};
+
+			int monthOfdays[] = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30,
+					31 };
+			int RmonthOfdays[] = { 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30,
+					31 };
 			if (isRyear) {
-				for(int i=0;i<month-1;i++){
-					counts+=monthOfdays[i];
+				for (int i = 0; i < month - 1; i++) {
+					counts += monthOfdays[i];
 				}
-				counts+=day;
-			}else {
-				for(int i=0;i<month-1;i++){
-					counts+=RmonthOfdays[i];
+				counts += day;
+			} else {
+				for (int i = 0; i < month - 1; i++) {
+					counts += RmonthOfdays[i];
 				}
-				counts+=day;
+				counts += day;
 			}
-			
-			System.out.println("你输入的日期为当年的第 "+counts+"天");
-			
+
+			System.out.println("你输入的日期为当年的第 " + counts + "天");
+
 		} catch (Exception e) {
 			System.out.println("您输入的格式有误请重新输入（yyyy-mm-dd）");
 		}
